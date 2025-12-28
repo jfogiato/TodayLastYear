38b74d2968fcb8893cb795ca901e4f7375ef9d13
jfogiato <joe.fogiato@gmail.com>
2025-12-28 16:11:03 -0600

    Initial Commit

---
+++ TodayLastYear/TodayLastYearApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayLastYearApp.swift
+//  TodayLastYear
+//
+//  Created by Joe Fogiato on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct TodayLastYearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodayLastYearTests/TodayLastYearTests.swift
@@ -0,0 +1,16 @@
+//
+//  TodayLastYearTests.swift
+//  TodayLastYearTests
+//
+//  Created by Joe Fogiato on 12/28/25.
+//
+
+import Testing
+
+struct TodayLastYearTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
